3d05328f5c305f93474ed22d6cc8952980f16082
Edsel Lopez <edsel.lopez@gmail.com>
2025-10-05 22:55:27 -0600

    Commit

--- crudMadata/Program.cs
+++ crudMadata/Program.cs
@@ -11,11 +11,10 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -24,7 +23,6 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
